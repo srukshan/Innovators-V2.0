c2537e1a5a04818e3473660f9b1cba20c51accc5
Sachith Rukshan <sachithrukshanmail@gmail.com>
2019-04-11 07:36:41 +0530

    build production

---
+++ client/build/precache-manifest.cc0bf5ce5191ab3b8495b0462461844b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c1f601923872a929d9a",
+    "url": "/static/js/main.de5f8444.chunk.js"
+  },
+  {
+    "revision": "1818927d133183ba5e0d",
+    "url": "/static/js/2.456df660.chunk.js"
+  },
+  {
+    "revision": "3c1f601923872a929d9a",
+    "url": "/static/css/main.6e42f119.chunk.css"
+  },
+  {
+    "revision": "1818927d133183ba5e0d",
+    "url": "/static/css/2.7f133e88.chunk.css"
+  },
+  {
+    "revision": "c2ec4330df029a9d9e47c1acde967239",
+    "url": "/index.html"
+  }
+];
